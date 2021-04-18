50e06e5e655b4809001761fa906ee1b58afe610b
丁腾飞 <wukong86372@gmail.com>
2021-04-18 21:56:56 +0800

    添加了dmm，KL函数（未实现）

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 import math
 
+
+class Entropy:
+    def __init__(self, entropy, dmm_size, dmm_complexity, dmm_interfacing, commits_msges):
+        super().__init__()
+        self.entropy = entropy
+        self.dmm_size = dmm_size
+        self.dmm_complexity = dmm_complexity
+        self.dmm_interfacing = dmm_interfacing
+        self.commits_msges = commits_msges
+    
+
+
 def get_entropy(files):
     total = sum(files.values())
     entropy = 0
@@ -7,3 +19,6 @@ def get_entropy(files):
         p = count/total
         entropy += p * math.log(p)
     return -entropy 
+
+def get_KL(files1, files2):
+    return 0
