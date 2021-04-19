f6568b3f86e687bfb02fd4cab513b3eb15e725fd
wukong <wukong86372@qq.com>
2021-04-19 15:51:47 +0800

    修复bug

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import math
 import scipy.stats
 
+
 class Entropy:
     def __init__(self, entropy, dmm_size, dmm_complexity, dmm_interfacing, commits_msges):
         super().__init__()
@@ -9,29 +10,31 @@ class Entropy:
         self.dmm_complexity = dmm_complexity
         self.dmm_interfacing = dmm_interfacing
         self.commits_msges = commits_msges
-    
+
+
 def get_entropy(files):
     total = sum(files.values())
     entropy = 0
     for count in files.values():
         p = count/total
         entropy += p * math.log(p)
-    return -entropy 
+    return -entropy
+
 
 def get_KL(files1, files2):
-    sum1 = sum(files1.values())
-    sum2 = sum(files2.values())
 
     for key in files2.keys():
         if key not in files1:
-            files1[key] = 0
-    
+            files1[key] = 0.0001
+
     for key in files1.keys():
         if key not in files2:
-            files2[key] = 0
-    
+            files2[key] = 0.0001
+
+    sum1 = sum(files1.values())
+    sum2 = sum(files2.values())
     prob1, prob2 = [], []
     for key in files1.keys():
         prob1.append(files1[key]/sum1)
         prob2.append(files2[key]/sum2)
-    return scipy.stats.entropy(prob1, prob2) 
+    return scipy.stats.entropy(prob1, prob2)
