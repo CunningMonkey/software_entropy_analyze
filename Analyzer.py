d2604d7e0443ee97d231bd438979485165071196
wukong <wukong86372@qq.com>
2021-05-06 16:16:33 +0800

    保留了三种频率

--- Analyzer.py
+++ Analyzer.py
@@ -20,17 +20,18 @@ class Analyzer:
         self.last_date = self.commits[len(self.commits)-1].committer_date
         self.auto = auto
         self.deltas = []
-        self.frequency = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
+        # self.frequency = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
+        self.frequency = [50, 100, 150]
         if auto:
             time_length = self.last_date - self.first_date
             for item in self.frequency:
                 self.deltas.append(time_length/item)
         else:
             self.deltas = deltas
-        
-        self.logger = logging.getLogger('Analyzer')
-        logging.basicConfig(level=logging.CRITICAL,filename='software.log',filemode='w')
 
+        self.logger = logging.getLogger('Analyzer')
+        logging.basicConfig(level=logging.CRITICAL,
+                            filename='software.log', filemode='w')
 
     def run(self):
 
@@ -45,7 +46,7 @@ class Analyzer:
         res = []
         commit_time1 = self.first_date
         files_last = []
-        
+
         for i in range(0, frequency):
             if i % 10 == 0:
                 self.logger.critical("now at :{}%{}\n".format(i, frequency))
@@ -64,18 +65,19 @@ class Analyzer:
 
             files_last = files_new
 
-            res.append((entropy, KL_entropy))
+            res.append((commit_time1.strftime('%Y-%m-%d-%H-%M'),
+                       commit_time2.strftime('%Y-%m-%d-%H-%M'), entropy, KL_entropy))
 
             if commit_time2 > self.last_date:
                 break
 
         return res
 
-    def writeToExcel(self, res, delta, columns=['entropy', 'KL']):
+    def writeToExcel(self, res, delta, columns=['start_time', 'end_time', 'entropy', 'KL']):
         if not os.path.exists("./{}_res".format(self.name)):
             os.mkdir("./{}_res".format(self.name))
         df = pandas.DataFrame(
-            res, columns=['entropy', 'KL'])
+            res, columns=columns)
         writer = pandas.ExcelWriter(
             './{}_res/time_{}.xlsx'.format(self.name, delta), engine='xlsxwriter')
         df.to_excel(writer, sheet_name='entropy', index=True)
